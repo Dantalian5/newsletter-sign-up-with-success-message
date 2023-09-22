b3c4cdbb5e35a8ccd455d5f8e857935307d40de1
Marcos Ernesto Planos Valenzuela <87524702+Dantalian5@users.noreply.github.com>
2023-09-22 10:37:55 +0200

    fix script & add states

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,3 +1,12 @@
 "use strict";
 const formBtn = document.getElementById("formButton");
 const formInput = document.getElementById("formEmail");
+const form = document.getElementById("myForm");
+
+formBtn.addEventListener("click", (event) => {
+	if (!formInput.validity.valid) {
+		form.classList.add("js-error");
+	} else {
+		form.classList.remove("js-error");
+	}
+});
