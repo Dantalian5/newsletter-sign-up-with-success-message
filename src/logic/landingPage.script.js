e751161d3ef8141729797e7cc3a02bd6183257fe
Marcos Ernesto Planos Valenzuela <87524702+Dantalian5@users.noreply.github.com>
2024-05-03 16:11:43 +0200

    Fix & Update code
    - Fix css styles (some styles not working properly as intended)
    - Fix responsive grid style to adapt original design
    - Update form validation (more simple and better structured)
    - Fix validation not showing error
    - Clean and fix project structure (improve for vercel deployment)

--- src/logic/landingPage.script.js
+++ src/logic/landingPage.script.js
@@ -1,17 +1,13 @@
-"use strict";
-const formBtn = document.getElementById("formButton");
-const formInput = document.getElementById("formEmail");
-const form = document.getElementById("myForm");
+'use strict';
+const form = document.getElementById('myForm');
 
-formBtn.addEventListener("click", (event) => {
-  if (!formInput.validity.valid) {
-     form.classList.add("logic-error");
-  } else {
-    form.classList.remove("logic-error");
-  }
-});
-
-document.querySelector("#myForm").addEventListener("submit", function (e) {
-  const email = document.querySelector("#formEmail").value;
-  sessionStorage.setItem("email", email);
+form.addEventListener('submit', event => {
+	const formInput = document.getElementById('formEmail');
+	if (!formInput.validity.valid) {
+		event.preventDefault();
+		form.classList.add('logic-error');
+	} else {
+		form.classList.remove('logic-error');
+		sessionStorage.setItem('email', formInput.value);
+	}
 });
